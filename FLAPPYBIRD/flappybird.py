329f11c8846956c190722ad130f277e8de71f0d1
tom0407 <s3974735@rmit.edu.vn>
2022-11-30 14:38:37 +0700

    Em mới nhớ ra là mình không thể call function trong hàm class nên em gộp chương trình thành hàm level và có những paramters như speed, acce để mình có thể tùy chỉnh tốc độ và gia tốc. Mình có thể thêm 1 số paramter vào function level để làm level sau nó khó hơn level trước ở nhiều đặc điểm. Trong hàm main em có cho người chơi chọn chế độ, mình nhập chế độ rồi mình mở tab game lên là chơi được ạ.

--- FLAPPYBIRD/flappybird.py
+++ FLAPPYBIRD/flappybird.py
@@ -1,138 +1,161 @@
 import pygame  # use pygame module
 from random import randint
 
-pygame.init()  # initialise pygame functions
-WIDTH, HEIGHT = 450, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  # set the width and height of the game screen
-pygame.display.set_caption("Flappy Bird")  # set the game caption
-running = True  # while the game is running, the while loop is infinity
-
 GREEN = (0, 200, 0)  # RGB color selector
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 
-clock = pygame.time.Clock()  # use clock function in pygame
 
-# constant variables
-TUBE_WIDTH = 50  # tube width
-TUBE_VELOCITY = 3  # tube speed
-TUBE_GAP = 150
+def level(speed, acce):
+    pygame.init()  # initialise pygame functions
+    width = 450
+    height = 600
+    screen = pygame.display.set_mode((width, height))  # set the width and height of the game screen
+    pygame.display.set_caption("Flappy Bird")  # set the game caption
+    clock = pygame.time.Clock()  # use clock function in pygame
+    running = True  # while the game is running, the while loop is infinity
 
-tube1_x = 600
-tube2_x = 800
-tube3_x = 1000
+    tube_width = 50  # tube width
+    tube_velocity = speed  # tube speed
+    acceleration = acce
+    tube_gap = 150
+    tube1_x = width + 300
+    tube2_x = width + 600
+    tube3_x = width + 900
+    tube_y = 0
+    tube1_height = randint(100, 400)
+    tube2_height = randint(100, 400)
+    tube3_height = randint(100, 400)
+    tube1_pass = False  # at the beginning, the bird does not pass the tube
+    tube2_pass = False
+    tube3_pass = False
 
-tube1_height = randint(100, 400)
-tube2_height = randint(100, 400)
-tube3_height = randint(100, 400)
+    bird_x = 50
+    bird_y = 400
+    bird_width = 35
+    bird_height = 35
+    bird_drop_velocity = 0
+    gravity = 0.3
 
-BIRD_X = 50
-bird_y = 400
-BIRD_WIDTH = 35
-BIRD_HEIGHT = 35
-bird_drop_velocity = 0
-GRAVITY = 0.3
+    score = 0
+    font = pygame.font.SysFont("san", 20)  # create font and size for text on screen
 
-score = 0
-font = pygame.font.SysFont("san", 20)  # create font and size for text on screen
+    pausing = False  # haven't lost the game
+    background_image = pygame.image.load("background.png")
+    bird_image = pygame.image.load("bird.png")
+    bird_image = pygame.transform.scale(bird_image, (bird_width, bird_height))
 
-tube1_pass = False  # at the beginning, the bird does not pass the tube
-tube2_pass = False
-tube3_pass = False
+    while running:  # game running process
+        clock.tick(60)  # 1 sec run 60 frames
+        screen.fill(GREEN)  # screen background color
+        screen.blit(background_image, (0, 0))
 
-pausing = False  # haven't lost the game
-background_image = pygame.image.load("background.png")
-bird_image = pygame.image.load("bird.png")
-bird_image = pygame.transform.scale(bird_image, (BIRD_WIDTH, BIRD_HEIGHT))
+        ''' Draw tubes '''
+        tube1_rect = pygame.draw.rect(screen, BLUE, (tube1_x, tube_y, tube_width, tube1_height))
+        tube2_rect = pygame.draw.rect(screen, BLUE, (tube2_x, tube_y, tube_width, tube2_height))
+        tube3_rect = pygame.draw.rect(screen, BLUE, (tube3_x, tube_y, tube_width, tube3_height))
 
-while running:  # game running process
-    clock.tick(60)  # 1 sec run 60 frames
-    screen.fill(GREEN)  # screen background color
-    screen.blit(background_image, (0, 0))
+        ''' Draw inverse tubes '''
+        tube1_rect_inv = pygame.draw.rect(screen, BLUE, (tube1_x, tube1_height + tube_gap, tube_width, height - tube1_height - tube_gap))
+        tube2_rect_inv = pygame.draw.rect(screen, BLUE, (tube2_x, tube2_height + tube_gap, tube_width, height - tube2_height - tube_gap))
+        tube3_rect_inv = pygame.draw.rect(screen, BLUE, (tube3_x, tube3_height + tube_gap, tube_width, height - tube3_height - tube_gap))
 
-    # draw tubes
-    tube1_rect = pygame.draw.rect(screen, BLUE, (tube1_x, 0, TUBE_WIDTH, tube1_height))
-    tube2_rect = pygame.draw.rect(screen, BLUE, (tube2_x, 0, TUBE_WIDTH, tube2_height))
-    tube3_rect = pygame.draw.rect(screen, BLUE, (tube3_x, 0, TUBE_WIDTH, tube3_height))
+        ''' Move faster '''
+        tube_velocity += acceleration
 
-    # draw tubes inverse
-    tube1_rect_inv = pygame.draw.rect(screen, BLUE, (tube1_x, tube1_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube1_height - TUBE_GAP))
-    tube2_rect_inv = pygame.draw.rect(screen, BLUE, (tube2_x, tube2_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube2_height - TUBE_GAP))
-    tube3_rect_inv = pygame.draw.rect(screen, BLUE, (tube3_x, tube3_height + TUBE_GAP, TUBE_WIDTH, HEIGHT - tube3_height - TUBE_GAP))
+        ''' Move tubes on Ox '''
+        tube1_x = tube1_x - tube_velocity
+        tube2_x = tube2_x - tube_velocity
+        tube3_x = tube3_x - tube_velocity
 
-    # move tubes on Ox
-    tube1_x = tube1_x - TUBE_VELOCITY
-    tube2_x = tube2_x - TUBE_VELOCITY
-    tube3_x = tube3_x - TUBE_VELOCITY
+        ''' Draw sand '''
+        sand_rect = pygame.draw.rect(screen, YELLOW, (0, height - 50, width, 50))
 
-    # draw sand
-    sand_rect = pygame.draw.rect(screen, YELLOW, (0, HEIGHT - 50, WIDTH, 50))
+        ''' Create new tubes when old tubes disappear'''
+        if tube1_x < -tube_width:
+            tube1_x = width + 100
+            tube1_height = randint(100, 400)
+            tube1_pass = False  # reset the tube pass
+        if tube2_x < -tube_width:
+            tube2_x = width + 100
+            tube2_height = randint(100, 400)
+            tube2_pass = False  # reset the tube pass
+        if tube3_x < -tube_width:
+            tube3_x = width + 100
+            tube3_height = randint(100, 400)
+            tube3_pass = False  # reset the tube pass
 
-    # create new tubes when old tubes disappear
-    if tube1_x < -TUBE_WIDTH:
-        tube1_x = 550
-        tube1_height = randint(100, 400)
-        tube1_pass = False  # reset the tube pass
-    if tube2_x < -TUBE_WIDTH:
-        tube2_x = 550
-        tube2_height = randint(100, 400)
-        tube2_pass = False  # reset the tube pass
-    if tube3_x < -TUBE_WIDTH:
-        tube3_x = 550
-        tube3_height = randint(100, 400)
-        tube3_pass = False  # reset the tube pass
+        ''' Draw bird'''
+        bird_rect = screen.blit(bird_image, (bird_x, bird_y))
 
-    # draw bird
-    bird_rect = screen.blit(bird_image, (BIRD_X, bird_y))
+        ''' Bird fall '''
+        bird_y += bird_drop_velocity
+        bird_drop_velocity += gravity
 
-    # bird falls
-    bird_y += bird_drop_velocity
-    bird_drop_velocity += GRAVITY
+        ''' Update score '''
+        score_txt = font.render("Score: " + str(score), True, BLACK)  # render text with color
+        screen.blit(score_txt, (5, 5))  # write text on screen
+        if tube1_x + tube_width <= bird_x and tube1_pass is False:  # 1 point for each time pass the tube
+            score += 1
+            tube1_pass = True
+        if tube2_x + tube_width <= bird_x and tube2_pass is False:
+            score += 1
+            tube2_pass = True
+        if tube3_x + tube_width <= bird_x and tube3_pass is False:
+            score += 1
+            tube3_pass = True
 
-    # update score
-    score_txt = font.render("Score: " + str(score), True, BLACK)  # render text with color
-    screen.blit(score_txt, (5, 5))  # write text on screen
-    if tube1_x + TUBE_WIDTH <= BIRD_X and tube1_pass is False:  # 1 point for each time pass the tube
-        score += 1
-        tube1_pass = True
-    if tube2_x + TUBE_WIDTH <= BIRD_X and tube2_pass is False:
-        score += 1
-        tube2_pass = True
-    if tube3_x + TUBE_WIDTH <= BIRD_X and tube3_pass is False:
-        score += 1
-        tube3_pass = True
+        ''' Check collision '''
+        for tube in [tube1_rect, tube2_rect, tube3_rect, tube1_rect_inv, tube2_rect_inv, tube3_rect_inv, sand_rect]:
+            if bird_rect.colliderect(tube):
+                pausing = True  # lost the game
+                tube_velocity = 0  # tubes stop moving
+                bird_drop_velocity = 0  # bird stops dropping
+                game_over_txt = font.render("GAME OVER, YOUR SCORE: " + str(score), True, RED)
+                screen.blit(game_over_txt, (100, 300))
+                press_space_txt = font.render("PRESS SPACE TO CONTINUE", True, RED)
+                screen.blit(press_space_txt, (100, 400))
 
-    # check collision
-    for tube in [tube1_rect, tube2_rect, tube3_rect, tube1_rect_inv, tube2_rect_inv, tube3_rect_inv, sand_rect]:
-        if bird_rect.colliderect(tube):
-            pausing = True  # lost the game
-            TUBE_VELOCITY = 0  # tubes stop moving
-            bird_drop_velocity = 0  # bird stops dropping
-            game_over_txt = font.render("GAME OVER, YOUR SCORE: " + str(score), True, RED)
-            screen.blit(game_over_txt, (100, 300))
-            press_space_txt = font.render("PRESS SPACE TO CONTINUE", True, RED)
-            screen.blit(press_space_txt, (100, 400))
+        ''' Set event such as mouseclick, keyboard buttons, quit'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # when you click on X button, it will exit the game
+                running = False
+            elif event.type == pygame.KEYDOWN:  # use keyboard
+                if event.key == pygame.K_SPACE:  # space button
+                    # reset
+                    if pausing:  # if player lost the game, reset all
+                        bird_y = 400
+                        tube_velocity = 3
+                        tube1_x = 600
+                        tube2_x = 800
+                        tube3_x = 1000
+                        score = 0
+                        pausing = False  # start again
+                    bird_drop_velocity = 0  # reset the bird's drop speed (no gravity)
+                    bird_drop_velocity -= 5  # make the bird jump
 
-    for event in pygame.event.get():  # set the event such as mouseclick, quit, keyboard clicks
-        if event.type == pygame.QUIT:  # when you click on X button, it will exit the game
-            running = False
-        elif event.type == pygame.KEYDOWN:  # use keyboard
-            if event.key == pygame.K_SPACE:  # space button
-                # reset
-                if pausing:  # if player lost the game, reset all
-                    bird_y = 400
-                    TUBE_VELOCITY = 3
-                    tube1_x = 600
-                    tube2_x = 800
-                    tube3_x = 1000
-                    score = 0
-                    pausing = False  # start again
+        pygame.display.flip()  # apply the colors changes to the screen
 
-                bird_drop_velocity = 0  # reset the bird's drop speed (no gravity)
-                bird_drop_velocity -= 5  # make the bird jump
+    pygame.quit()  # finish using pygame
 
-    pygame.display.flip()  # apply the colors changes to the screen
 
-pygame.quit()  # finish using pygame
+def main():
+    while True:
+        lvl = input("Enter the level you want (easy, medium, hard): ")
+        if lvl == "easy":
+            level(1, 0.001)  # easy
+            break
+        elif lvl == "medium":
+            level(2, 0.002)  # medium
+            break
+        elif lvl == "hard":
+            level(3, 0.003)  # hard
+            break
+        else:
+            print("Invalid syntax, please enter again: ")
+            continue
+
+
+main()
