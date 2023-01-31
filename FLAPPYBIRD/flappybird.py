b092de0cb70e4647eac3d30e4d5ae13430b6e4c3
NhatNguyen3001 <dangnhat54321@gmail.com>
2023-01-31 13:41:38 +0700

    push 31-01-2022

--- FLAPPYBIRD/flappybird.py
+++ FLAPPYBIRD/flappybird.py
@@ -213,6 +213,8 @@ def level(speed, acceleration, tube_change):
 def menu():
     pygame.init()
 
+    game_level = "easy"
+
     # create game window
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
@@ -231,22 +233,16 @@ def menu():
     TEXT_COL = (255, 255, 255)
 
     # load button images
-    start_img = pygame.image.load("images/START.png").convert_alpha()
-    mode_img = pygame.image.load("images/MODE.png").convert_alpha()
     quit_img = pygame.image.load("images/QUIT.png").convert_alpha()
     easy_img = pygame.image.load('images/EASY.png').convert_alpha()
     medium_img = pygame.image.load('images/MEDIUM.png').convert_alpha()
     hard_img = pygame.image.load('images/HARD.png').convert_alpha()
-    back_img = pygame.image.load('images/BACK.png').convert_alpha()
 
     # create button instances
-    start_button = button.Button(250, 150, start_img, 1)
-    mode_button = button.Button(250, 250, mode_img, 1)
     quit_button = button.Button(250, 350, quit_img, 1)
     easy_button = button.Button(250, 100, easy_img, 1)
     medium_button = button.Button(250, 200, medium_img, 1)
     hard_button = button.Button(250, 300, hard_img, 1)
-    back_button = button.Button(250, 400, back_img, 1)
 
     def draw_text(text, text_font, text_col, x, y):
         img = text_font.render(text, True, text_col)
@@ -259,29 +255,7 @@ def menu():
         screen.fill((52, 78, 91))
 
         # check if game is paused
-        if game_paused:
-            # check menu state
-            if menu_state == "main":
-                # draw pause screen buttons
-                if start_button.draw(screen):
-                    game_paused = False
-                if mode_button.draw(screen):
-                    menu_state = "options"
-                if quit_button.draw(screen):
-                    run = False
-            # check if the options menu is open
-            if menu_state == "options":
-                # draw the different options buttons
-                if easy_button.draw(screen):
-                    level(1, 0.001, 0)  # easy
-                if medium_button.draw(screen):
-                    level(2, 0.004, 0)  # medium
-                if hard_button.draw(screen):
-                    level(3, 0.002, 0.6)  # hard
-                if back_button.draw(screen):
-                    menu_state = "main"
-        else:
-            draw_text("Press SPACE to enter the game", font, TEXT_COL, 60, 250)
+
 
         # event handler
         for event in pygame.event.get():
